07e6fde0ccb7e67717cbb624d1c630c2ed55a42b
zydxhs <johnsmithcry@163.com>
2020-10-01 21:38:01 +0800

    Add document and fix bug

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,34 @@
+//! Provides macros for fetching the package's compile date and time.
+//!
+//! # Example
+//!
+//! ```
+//! use pkg_compile_time::*;
+//! fn main(){
+//!     println!("Compile date is: {}", pkg_compile_date!());
+//!     println!("Compile time is: {}", pkg_compile_time!());
+//! }
+//! ```
+//! The Output is:
+//! ```
+//! Compile date is: 2020-10-01
+//! Compile time is: 19:58:49
+//! ```
 use proc_macro::TokenStream;
 use quote::*;
 
+/// Get the package's compile date.
 #[proc_macro]
 pub fn pkg_compile_date(_input: TokenStream) -> TokenStream {
-    TokenStream::from(quote!(&chrono::Local::now().format("%Y-%m-%d").to_string()))
+    use chrono::Local;
+    let date = Local::now().format("%Y-%m-%d").to_string();
+    TokenStream::from(quote!(#date))
 }
 
+/// Get the package's compile time.
 #[proc_macro]
 pub fn pkg_compile_time(_input: TokenStream) -> TokenStream {
-    TokenStream::from(quote!(&chrono::Local::now().format("%H:%M:%S").to_string()))
+    use chrono::Local;
+    let time = Local::now().format("%H:%M:%S").to_string();
+    TokenStream::from(quote!(#time))
 }
